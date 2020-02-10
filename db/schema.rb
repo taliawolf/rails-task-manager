e4f493c9f25b90b410baad225b1e22a036def625
talia wolf <taliamwolf@gmail.com>
2020-02-10 16:45:47 +0200

    done

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_104004) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
